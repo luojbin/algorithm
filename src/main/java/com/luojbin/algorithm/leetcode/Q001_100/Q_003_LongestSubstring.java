230d8df3e708a952a20b7840d76c886bdb15b8f2
luojbin <z1_2x@msn.com>
2022-03-29 23:03:49 +0800

    滑动窗口例题

--- src/main/java/com/luojbin/algorithm/leetcode/Q001_100/Q_003_LongestSubstring.java
+++ src/main/java/com/luojbin/algorithm/leetcode/Q001_100/Q_003_LongestSubstring.java
@@ -1,9 +1,8 @@
 package com.luojbin.algorithm.leetcode.Q001_100;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 public class Q_003_LongestSubstring {
     public static void main(String[] args) {
@@ -36,5 +35,46 @@ public class Q_003_LongestSubstring {
             maxcount = tempList.size();
         }
         return maxcount;
+    }
+
+    @Test
+    public void testSlideWindow() {
+        // 原始字符串
+        String s = "hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        System.out.println(getLengthBySlideWindow(s));
+    }
+
+    private int getLengthBySlideWindow(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        // ascii码, 256个可能字符
+        int[] flags= new int[256];
+        Arrays.fill(flags, -1);
+        // 转换成数组
+        char[] chars = s.toCharArray();
+        // 滑动窗口的两个指针
+        int start = 0, end=0;
+        // 总长度
+        int length = s.length();
+        // 目标结果, 子串长度, 至少是1;
+        int result = 1;
+        while(end < length) {
+            int letter = chars[end];
+            int beforeIdx = flags[letter];
+            if (beforeIdx != -1 && beforeIdx >= start) {
+                // 重复字符, 且在有效子串内
+                // 将 start 移动到重复字符的后一位
+                start = beforeIdx + 1;
+            }
+            // 更新当前字符的坐标
+            flags[letter] = end;
+            int currentLength = end - start + 1;
+            if (currentLength > result) {
+                result = currentLength;
+            }
+            end++;
+        }
+        return result;
     }
 }
---
+++ src/main/java/com/luojbin/algorithm/leetcode/Q001_100/Q_026_removeRepeatNum.java
@@ -0,0 +1,33 @@
+package com.luojbin.algorithm.leetcode.Q001_100;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Q_026_removeRepeatNum {
+
+    @Test
+    public void test() {
+        int[] nums = {1,2,3,3,5,5,6,7,8,8,9,9};
+        System.out.println(removeRepeat(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public int removeRepeat(int[] nums) {
+        int length = nums.length;
+        if (length == 0 || length == 1) {
+            return length;
+        }
+        int k = 1;
+        int t;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] > nums[k-1]) {
+                t = nums[i];
+                nums[i] = nums[k];
+                nums[k] = t;
+                k++;
+            }
+        }
+        return k;
+    }
+}
